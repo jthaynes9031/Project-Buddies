c3022b32388799b1c971706119c2e32d5cc01c07
jthaynes9031@gmail.com <jthaynes9031@gmail.com>
2021-08-07 20:33:48 -0400

    starting the training and performance interface

--- src/main/java/midtermConnection/TrainingandPerformance.java
+++ src/main/java/midtermConnection/TrainingandPerformance.java
@@ -1,5 +1,13 @@
 package midtermConnection;
 
 public interface TrainingandPerformance {
-
+	
+	/*
+	 * declare sql statements as strings 
+	 * IMPORTANT change connections from employee_id to employee_number in sql
+	 * 
+	 */
+	
+	String SET_PERFORMANCE = "Insert into performance (performance, employee_id) value(?,?)";
+	String UPDATE_PERFORMANCE = "Update performance set performance = ? where employee_id = ?";
 }
