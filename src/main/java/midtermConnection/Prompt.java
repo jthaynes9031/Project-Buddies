99148e9624828a821accd550f02f901a02ae3f67
jthaynes9031@gmail.com <jthaynes9031@gmail.com>
2021-08-11 19:36:23 -0400

    prompt changes, place holder so we can get to GUI as quickly as possible

--- src/main/java/midtermConnection/Prompt.java
+++ src/main/java/midtermConnection/Prompt.java
@@ -9,46 +9,82 @@ public class Prompt {
 		Employee emp = new Employee();
 		Payroll pay = new Payroll();
 		Benefits ben = new Benefits();
+		Fin fin = new Fin();
 		Scanner input = new Scanner(System.in);
+		
+		
+		String divider = "===========================================================================================================================";
 		
 		boolean quit = false;
 		while(!quit) {
-			System.out.println("type 'add' to insert into database, type 'find' to search database, type 'update' to do update methods, or type 'delete' to delete items from database");
+			System.out.println("type instructions if youre new");
 			String choose = input.nextLine();
 			
-			if(choose.equals("add")) {
-				System.out.println("type either one of these commands");
-				System.out.println("add employee");
-				System.out.println("add payroll");
-				System.out.println("add benefits");
-				System.out.println("add performance");
-				System.out.println("add training");
-				System.out.println("add position");
+			
+			if(choose.equals("instructions")) {				
+				System.out.println("type 'add' to insert into database");
+				System.out.println("type 'find' to search database");
+				System.out.println("type 'update' to update items in database"); 
+				System.out.println("type 'delete' to delete items in database");
+			}
+
+			
+			else if(choose.equals("add")) {
+				System.out.println("Type either one of these commands");
+				System.out.println(divider);
+				System.out.println("'add employee'");
+				System.out.println("'add payroll'");
+				System.out.println("'add benefits'");
+				System.out.println("'add performance'");
+				System.out.println("'add training'");
+				System.out.println("'add position'");
+				System.out.println(divider);
 			}
 			else if(choose.equals("find")) {
 				System.out.println("type either one of these commands");
+				System.out.println(divider);
 				System.out.println("find employee");
 				System.out.println("find pay info");
 				System.out.println("find benefits");
+				System.out.println(divider);
 			}
 			else if(choose.equals("delete")) {
 				System.out.println("type either one of these commands");
+				System.out.println(divider);
 				System.out.println("delete employee");
 				System.out.println("delete pay info");
 				System.out.println("delete benefits");
+				System.out.println(divider);
 			}
 			else if(choose.equals("update")) {
 				System.out.println("type either one of these commands");
+				System.out.println(divider);
 				System.out.println("update employee");
 				System.out.println("update pay info");
 				System.out.println("update benefits");
 				System.out.println("update performance");
 				System.out.println("update position");
 				System.out.println("update training");
+				System.out.println(divider);
 			}
 	
 			else if(choose.equals("add employee")) {	
 				emp.addEmployee();	
+			}
+			else if(choose.equals("add benefits")) {				
+				ben.addBenefits();
+			}
+			else if(choose.equals("add pay")) {
+				pay.setPayrate();				
+			}
+			else if(choose.equals("add position")) {
+				fin.addPosition();;				
+			}
+			else if(choose.equals("add training")) {
+				fin.addTraining();				
+			}
+			else if(choose.equals("add direct deposit")) {
+				pay.deposit();				
 			}
 			else if(choose.equals("find employee")) {
 				emp.findEmployee();	
@@ -56,40 +92,37 @@ public class Prompt {
 			else if(choose.equals("find pay info")) {
 				pay.findPayAll();
 			}
-			else if(choose.equals("delete employee")) {	
-				emp.deleteEmployee();
-			}
-			else if(choose.equals("update employee")) {
-				emp.updateEmployee();
-			}
-			else if(choose.equals("delete pay")) {
-				pay.deletePay();
-			}
-			else if(choose.equals("add pay")) {
-				pay.setPayrate();				
-			}
-			else if(choose.equals("add direct deposit")) {
-				pay.deposit();				
-			}
-			else if(choose.equals("update Pay")) {
-				pay.updatePay();				
-			}
 			else if(choose.equals("find pay")) {
 				pay.findPayrate();				
-			}
-			else if(choose.equals("add benefits")) {				
-				ben.addBenefits();
 			}
 			else if(choose.equals("find benefits")) {				
 				ben.findBenefits();
 			}
-			else if(choose.equals("delete benefits")) {				
-				ben.deleteBenefits();
+			else if(choose.equals("update employee")) {
+				emp.updateEmployee();
+			}
+			else if(choose.equals("update Pay info")) {
+				pay.updatePay();				
+			}
+			else if(choose.equals("update position")) {
+				fin.updatePosition();				
+			}
+			else if(choose.equals("update training")) {
+				fin.updateTraining();				
 			}
 			else if(choose.equals("update benefits")) {				
 				ben.updateBenefits();
 			}
-			if(choose.equals("clock out")) {
+			else if(choose.equals("delete pay")) {
+				pay.deletePay();
+			}
+			else if(choose.equals("delete employee")) {	
+				emp.deleteEmployee();
+			}
+			else if(choose.equals("delete benefits")) {				
+				ben.deleteBenefits();
+			}
+			else if(choose.equals("clock out")) {
 				emp.employeeOut();
 				System.out.println("Youre out");
 				quit = true;
